c392a5e4d21820d844d5f45a09549dd995d6c37a
Saka Manish <sakamanish8@gmail.com>
2025-06-25 15:39:53 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -2,10 +2,6 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        int a=3;
-        int b=5;
-
-        int c=a+b;
-        System.out.println(c);
+        System.out.println("Hello!");
     }
 }
